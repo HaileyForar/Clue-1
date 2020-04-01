2a285ca76402375dfd94c3f025bc65d22fe315fd
local_admin <local_admin@10.22.22.116>
2020-04-01 14:42:46 -0600

    Test Change - simply added print statement

--- Clue/src/HelloWorld.java
+++ Clue/src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("Hey everyone, here is my test change. Just want to make sure everything is working. Stay healthy lovely people!");
 
 	}
 
